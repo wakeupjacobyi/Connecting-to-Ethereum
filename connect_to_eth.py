394fe572adc09dba4925cbf220962433b896f6b3
wakeupjacobyi <jacob.yi0913@gmail.com>
2024-10-21 22:58:23 -0400

    Fix indentation

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -27,19 +27,19 @@ def connect_with_middleware(contract_json):
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
 	url = "https://bsc-testnet.infura.io/v3/b55c529c669646afadfe3bbda28d03b2"
-  w3 = Web3(HTTPProvider(url))
-  assert w3.is_connected(), f"Failed to connect to provider at {url}"
+  	w3 = Web3(HTTPProvider(url))
+  	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
 
-  # Inject the middleware for Proof of Authority (PoA) consensus
-  w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+	# Inject the middleware for Proof of Authority (PoA) consensus
+	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-  # Create a contract object using the address and ABI
-  contract = w3.eth.contract(address=address, abi=abi)
+	# Create a contract object using the address and ABI
+	contract = w3.eth.contract(address=address, abi=abi)
 
 	return w3, contract
 
